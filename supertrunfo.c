104eaaf6f4f4103a695fd87fe575a3f0a377d925
MVinicius <vmarcio469@gmail.com>
2025-04-21 21:12:49 -0300

    retirada de acentos

--- supertrunfo.c
+++ supertrunfo.c
@@ -12,77 +12,77 @@ int main()
     int pontosTuristicos1, pontosTuristicos2;
 
     // Introdução pro jogo
-    printf("Bem-vindo ao Super Trunfo das Cidades! Bora cadastrar suas cartas:\n");
+    printf("Bem-vindo ao Super Trunfo das Cidades! Cadastre suas cartas:\n");
 
     // Primeira carta
     printf("Primeira carta:\n");
 
-    printf("Digite o estado (letra de A até H): \n");
+    printf("Digite o estado (letra de A ate H): \n");
     scanf(" %c", &estado1);
 
-    printf("Código da carta (ex: A01): \n");
+    printf("Codigo da carta (ex: A01): \n");
     scanf("%s", codigo1);
 
     printf("Nome da cidade: \n");
     scanf(" %s", nomeCidade1);
 
-    printf("População: \n");
+    printf("Populacao: \n");
     scanf("%d", &populacao1);
 
-    printf("Área (em km²): \n");
+    printf("Area (em km²): \n");
     scanf("%f", &area1);
 
-    printf("PIB (em bilhões): \n");
+    printf("PIB (em bilhoes): \n");
     scanf("%f", &pib1);
 
-    printf("Número de pontos turísticos: \n");
+    printf("Número de pontos turisticos: \n");
     scanf("%d", &pontosTuristicos1);
 
     // Exibindo a primeira carta
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado1);
-    printf("Código: %s\n", codigo1);
+    printf("Codigo: %s\n", codigo1);
     printf("Cidade: %s\n", nomeCidade1);
     printf("População: %d\n", populacao1);
-    printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f bilhões de reais\n", pib1);
-    printf("Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Area: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhoes de reais\n", pib1);
+    printf("Pontos Turisticos: %d\n", pontosTuristicos1);
 
     // Segunda carta
     printf("\nSegunda carta:\n");
 
-    printf("Digite o estado (letra de A até H): \n");
+    printf("Digite o estado (letra de A ate H): \n");
     scanf(" %c", &estado2);
 
-    printf("Código da carta (ex: B02): \n");
+    printf("Codigo da carta (ex: B02): \n");
     scanf("%s", codigo2);
 
     printf("Nome da cidade: \n");
     scanf("%s", nomeCidade2);
 
-    printf("População: \n");
+    printf("Populacao: \n");
     scanf("%d", &populacao2);
 
-    printf("Área (em km²): \n");
+    printf("Area (em km²): \n");
     scanf("%f", &area2);
 
-    printf("PIB (em bilhões): \n");
+    printf("PIB (em bilhoes): \n");
     scanf("%f", &pib2);
 
-    printf("Número de pontos turísticos: \n");
+    printf("Numero de pontos turisticos: \n");
     scanf("%d", &pontosTuristicos2);
 
     // Exibindo a segunda carta
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
-    printf("Código: %s\n", codigo2);
+    printf("Codigo: %s\n", codigo2);
     printf("Cidade: %s\n", nomeCidade2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f bilhões de reais\n", pib2);
-    printf("Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhoes de reais\n", pib2);
+    printf("Pontos Turisticos: %d\n", pontosTuristicos2);
 
-    printf("\nCadastro finalizado! Agora é só jogar. Boa sorte no Super Trunfo!\n");
+    printf("\nCadastro finalizado! Agora e so jogar. Boa sorte no Super Trunfo!\n");
 
     return 0;
 }
